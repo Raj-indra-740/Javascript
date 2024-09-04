aba01fd177f44f5031c951e8a349f3eadf287808
rajendra <rajendra@appscrip.co>
2024-09-04 15:30:33 +0530

    operators

---
+++ JS-Fundamentals/07-operators.js
@@ -0,0 +1,34 @@
+console.log('2' + '2')
+console.log( 2 + '2')
+console.log(2 + 2 + '2')
+console.log(2 + '2' + 2)
+console.log('2' + 2 + 2)
+
+
+console.log(+"raj")
+console.log(+"")
+console.log(+null)
+
+
+
+let a = 2;
+console.log('a',a)
+
+let b = ++a;
+console.log('a',a)
+console.log('b',b)
+
+let c = a++;
+
+
+console.log('a',a)
+console.log('c',c)
+
+console.log('a', a)
+
+
+console.log(""+ 1 + 0)
+console.log("" - 1 + 0)
+console.log(true + false)
+console.log(2/'2')
+console.log('2'*'2')
