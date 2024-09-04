0c90b040a49282dc42445612be7755f37042d294
rajendra <rajendra@appscrip.co>
2024-09-04 13:21:16 +0530

    js variables

---
+++ JS-Fundamentals/03-varible.js
@@ -0,0 +1,14 @@
+let firstName;
+firstName = 'Rajendra';
+
+let lastName = 'Chaudhari';
+
+console.log( firstName +' ' + lastName)
+
+let age = 21;
+console.log(age);
+age  = 22; 
+console.log(age)
+
+age = firstName;
+console.log(age)
