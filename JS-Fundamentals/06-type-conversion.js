21dd8041cfe798bc6d429d7ffa80ebb21329cfb2
rajendra <rajendra@appscrip.co>
2024-09-04 15:30:21 +0530

    typer oncverson

---
+++ JS-Fundamentals/06-type-conversion.js
@@ -0,0 +1,16 @@
+let num = Number(22)
+console.log(num)
+
+let undefine = Number(undefined);
+let NULL = Number(null);
+let boolean = Number(true);
+let str = Number('raj');
+
+console.log(undefine)
+console.log(NULL)
+console.log(boolean)
+console.log(str)
+
+
+// All empty or 0 consisting type are converted in false
+// undefined, null, '',0 --> false
