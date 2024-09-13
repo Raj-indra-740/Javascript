aea4b6219bc62e00dcc3fb632b53a72743b25d42
rajendra <rajendra@appscrip.co>
2024-09-13 12:47:50 +0530

    variable name change

--- JS-Data-Types/01-basics.js
+++ JS-Data-Types/01-basics.js
@@ -1,7 +1,7 @@
 //The “object wrappers” are different for each primitive type and are called: String, Number, Boolean, Symbol and BigInt. Thus, they provide different sets of methods.
 
-let str = "Hello";
+let string = "Hello";
 
-str.test = 5; // in strict mode it will give error
+string.test = 5; // in strict mode it will give error
 
-console.log(str.test);
+console.log(string.test);
